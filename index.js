973423b9bc0b6adbd89500d57892b5335c0decf0
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-01-30 09:27:28 +0000

    los dos metodos sirven

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ function bubble_Sort(a)
 }
 
 console.log(bubble_Sort([12,345,4,546,122,84,98,64,9,1,3223,455,23,234,213]));
+
+
+//////////////////////////////////////
+
+
+
+const selection = arr => {
+  const swap = (list, a, b) => [list[a], list[b]] = [list[b], list[a]];
+
+  arr.forEach((item, i) => {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    };
+    swap(arr, i, min);
+  });
+
+  return arr;
+};
+
+console.log(selection([12,345,4,546,122,84,98,64,9,1,3223,455,23,234,213]));
