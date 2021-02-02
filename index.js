90ad8e77243b82ac329cbea3a730bc5ac4e4b5fe
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-02 04:49:38 +0000

    arrays

--- index.js
+++ index.js
@@ -5,14 +5,59 @@ var cantidadcartas = document.getElementById("texto");
 var mostrar = document.getElementById("btn3");
 
 mostrar.addEventListener("click", ()=>{
+
+
+ /////////////////cantidadcartas va ser la cantidad que escribe el usuario
+
     cantidadcartas=cantidadcartas.value;
-alert(cantidadcartas);
+//alert(cantidadcartas);
+for (let i=0;i<cantidadcartas;i++){
+
+console.log(i);
+
+var Arraynumeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", 'Q', 'K'];
+//                    corazon   espada     trebol   diamante
+var Arraysimbolos = ["&#x2665", '\u2660', '&#x2663', '&#x2666'];
+
+var numeros = Math.floor(Math.random() * (12));
+var simbolos = Math.floor(Math.random() * (4));
+
+//console.log(Arraynumeros= [numeros]);
+
+//let sign = window.prompt("cuadro");
+//falta poner colores 
+
+if (Arraysimbolos[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
+    let div1 = document.querySelector('div1');
+    let div3 = document.querySelector('div3');
+    div1.style.color = 'red';
+    div3.style.color = 'red';
+}
+
+document.querySelector("div1").innerHTML = Arraysimbolos[simbolos]
+document.querySelector("div2").innerHTML = Arraynumeros[numeros]
+document.querySelector("div3").innerHTML = Arraysimbolos[simbolos]
+    
+}
+
+
 })
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////// prueba for
+
+
+///////termina el for
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////termina prueba 
 
 
 /////////////////////////////////////////////////
-///empieza deslñiegue de cartas
+///empieza despliegue de cartas
 
 var Arraynumeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", 'Q', 'K'];
 //                    corazon   espada     trebol   diamante
