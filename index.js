8106d81c26fed902f928bf6511e8fd160cffb894
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-03 04:21:38 +0000

    2 prueba de sort

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Arraynumeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", 'Q', 'K'];
 //                    corazon   espada     trebol   diamante
 var Arraysimbolos = ["&#x2665", '\u2660', '&#x2663', '&#x2666'];
 nuevonum = [];
-nuevosim=[];
+nuevosim = [];
 
 var cantidadcartas = document.getElementById("texto");
 var mostrar = document.getElementById("btn3");
@@ -19,20 +19,20 @@ mostrar.addEventListener("click", () => {
     for (let i = 0; i < cantidadcartas; i++) {
 
         var numeros = Math.floor(Math.random() * (13));
-         nuevonum[i]=numeros;
-         console.log( nuevonum[i]);         
+        nuevonum[i] = numeros;
+        console.log(nuevonum[i]);
         var simbolos = Math.floor(Math.random() * (4));
-          
+
 
         //let sign = window.prompt("cuadro");
         //falta poner colores 
-      
+
         let padre = document.createElement("div");
         let arriba = document.createElement("div");
         let medio = document.createElement("div");
         let abajo = document.createElement("div");
 
-        
+
 
         if (Arraysimbolos[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
             arriba.style.color = 'red';
@@ -44,7 +44,7 @@ mostrar.addEventListener("click", () => {
         arriba.innerHTML = Arraysimbolos[simbolos];
         medio.innerHTML = Arraynumeros[numeros];
         abajo.innerHTML = Arraysimbolos[simbolos];
-        
+
         ///llama al style creado del css
         padre.classList.add("creado");
         arriba.classList.add("div1");
@@ -58,188 +58,71 @@ mostrar.addEventListener("click", () => {
 
         cards.appendChild(padre);
 
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////ordenamiento burbuja 
+    let btn = document.querySelector('#btn');
+    btn.addEventListener('click', bubble_Sort);
 
-
-        ////////////////
-          ///////////////////////////////////////////////////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////////////////////
- ///////////////////////pruebas
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////ordenamiento burbuja 
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', bubble_Sort);
-
-function bubble_Sort() {
-    var swapp;
-    var n = nuevonum.length - 1;
-    var x = nuevonum;
-    console.log(nuevonum);
-    do {
-        swapp = false;
-        for (var i = 0; i < n; i++) {
-            if (x[i] > x[i + 1]) {
-                var temp = x[i];
-                x[i] = x[i + 1];
-                x[i + 1] = temp;
-                swapp = true;
+    function bubble_Sort() {
+        var swapp;
+        var n = nuevonum.length - 1;
+        var x = nuevonum;
+        console.log(nuevonum);
+        do {
+            swapp = false;
+            for (var j = 0; j < n; j++) {
+                if (x[j] > x[j + 1]) {
+                    var temp = x[j];
+                    x[j] = x[j + 1];
+                    x[j + 1] = temp;
+                    swapp = true;
+                }
             }
+            n--;
+        } while (swapp);
+
+        let lista = document.querySelector('#lista-numeros');
+        for (let k = 0; k < nuevonum.length; k++) {
+            let item = document.createElement('li');
+            //item.innerHTML = nuevonum[k];
+
+            item.innerHTML = Arraynumeros[nuevonum[k]];
+            lista.appendChild(item);
         }
-        n--;
-    } while (swapp);
-
-    let lista = document.querySelector('#lista-numeros');
-    for (let k = 0; k < nuevonum.length; k++) {
-        let item = document.createElement('li');
-        item.innerHTML = nuevonum[k];
-        lista.appendChild(item);
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////termina burbuja
 
-
-    //  console.log(nuevo);
-
-
-}
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////selector sort
-const selectSort = () => {
-    let min = 0;
-    while (min < nuevonum.length - 1) {
-        for (let i = min + 1; i < nuevonum.length - 1; i++) {
-            if (nuevonum[min] > nuevonum[i]) {
-                let aux = nuevonum[min];
-                nuevonum[min] = nuevonum[i];
-                nuevonum[i] = aux;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////selector sort
+    const selectSort = () => {
+        let min = 0;
+        while (min < nuevonum.length - 1) {
+            for (let m = min + 1; m < nuevonum.length; m++) {
+                if (nuevonum[min] > nuevonum[m]) {
+                    let aux = nuevonum[min];
+                    nuevonum[min] = nuevonum[m];
+                    nuevonum[m] = aux;
+                }
             }
+            min++;
         }
-        min++;
-    }
-    let lista2 = document.querySelector('#lista-numeros2');
-    for (let i = 0; i < nuevonum.length; i++) {
-        let item = document.createElement('li');
-        item.innerHTML = nuevonum[i];
-        lista2.appendChild(item);
-    }
-};
+        let lista2 = document.querySelector('#lista-numeros2');
+        for (let l = 0; l < nuevonum.length; l++) {
+            let item = document.createElement('li');
+            //item.innerHTML = nuevonum[l];
+            item.innerHTML = Arraynumeros[nuevonum[l]];
+            lista2.appendChild(item);
 
-
-let btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click', selectSort);
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    }
-
-
- ///////////////////////////////////////////////////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////////////////////
- ///////////////////////pruebas
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////ordenamiento burbuja 
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', bubble_Sort);
-
-function bubble_Sort() {
-    var swapp;
-    var n = nuevonum.length - 1;
-    var x = nuevonum;
-    console.log(nuevonum);
-    do {
-        swapp = false;
-        for (var i = 0; i < n; i++) {
-            if (x[i] > x[i + 1]) {
-                var temp = x[i];
-                x[i] = x[i + 1];
-                x[i + 1] = temp;
-                swapp = true;
-            }
         }
-        n--;
-    } while (swapp);
-
-    let lista = document.querySelector('#lista-numeros');
-    for (let k = 0; k < nuevonum.length; k++) {
-        let item = document.createElement('li');
-        item.innerHTML = nuevonum[k];
-        lista.appendChild(item);
-    }
+    };
 
 
-    //  console.log(nuevo);
+    let btn2 = document.querySelector('#btn2');
+    btn2.addEventListener('click', selectSort);
 
-
-}
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    /////////////termina ordenar select
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 });
-
-
-for (contador = 0; contador < cantidadcartas; contador++) {
-
-    //var numeros = Math.floor(Math.random() * (12));
-
-    var desplegar = nuevo[contador];
-    console.log(desplegar);
-    //nuevo[contador] = Arraynumeros[numeros];
-}
-
-
-
-
-///////////funcion que recibe un numero del usuario 
-function recibir() {
-    //valor es el numero escrito por el usuario
-    var valor = document.getElementById("texto").value;
-
-
-    //document.getElementById("txt").innerHTML=valor;        
-
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////ordenamiento burbuja 
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', bubble_Sort);
-
-function bubble_Sort() {
-    var swapp;
-    var n = nuevonum.length - 1;
-    var x = nuevonum;
-    console.log(nuevonum);
-    do {
-        swapp = false;
-        for (var i = 0; i < n; i++) {
-            if (x[i] > x[i + 1]) {
-                var temp = x[i];
-                x[i] = x[i + 1];
-                x[i + 1] = temp;
-                swapp = true;
-            }
-        }
-        n--;
-    } while (swapp);
-
-    let lista = document.querySelector('#lista-numeros');
-    for (let i = 0; i < nuevonum.length; i++) {
-        let item = document.createElement('li');
-        item.innerHTML = nuevonum[i];
-        lista.appendChild(item);
-    }
-
-
-    //  console.log(nuevo);
-
-
-}
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
