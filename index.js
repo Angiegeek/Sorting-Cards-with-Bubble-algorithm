b50ae2cfedcb41aed0b60c6eb6dc8156ff23d6dc
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-06 02:29:30 +0000

    actulizacion

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 var Arraynumeros = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", 'Q', 'K'];
 //                    corazon   espada     trebol   diamante
-var Arraysimbolos = ["&#x2665", '\u2660', '&#x2663', '&#x2666'];
+//var Arraysimbolos = [1, 2, 3, 4];
+var Arraysimbolos = ["&#x2665", "\u2660", '&#x2663', '&#x2666'];
 nuevonum = [];
 nuevosim = [];
 
@@ -10,6 +11,7 @@ var mostrar = document.getElementById("btn3");
 var cards = document.getElementById("cards");
 
 //asignacion de funciones
+
 mostrar.addEventListener("click", () => {
 
     /////////////////cantidadcartas va ser la cantidad que escribe el usuario
@@ -22,8 +24,12 @@ mostrar.addEventListener("click", () => {
         nuevonum[i] = numeros;
         //console.log(nuevonum[i]);
         var simbolos = Math.floor(Math.random() * (4));
-         nuevosim[i] = simbolos;
-         //console.log(nuevosim);
+        console.log(simbolos);
+         //console.log(simbolos);
+        //nuevosim[i] = Arraysimbolos[simbolos];
+        //console.log(Arraysimbolos[simbolos]);
+
+        //console.log(nuevosim);
         //let sign = window.prompt("cuadro");
         //falta poner colores 
 
@@ -91,20 +97,55 @@ mostrar.addEventListener("click", () => {
       
             item.innerHTML = Arraynumeros[nuevonum[k]];
             
-           console.log(nuevonum[k]);
+          // console.log(nuevonum[k]);
            // console.log(nuevosim[k]);
-            nuevosim[k]=nuevosim[nuevonum];
+            nuevosim[k]=nuevosim[nuevonum[k]];
             //console.log(nuevosim);
             item2.innerHTML = nuevosim[nuevonum[k]];
             
-            lista.appendChild(item);
+            //lista.appendChild(item);
             //lista.appendChild(item2);
       
 
               //  console.log(nuevonum[k]);
                 
-       
+       /////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////pruebas
+    
+        let padre = document.createElement("div");
+        let arriba = document.createElement("div");
+        let medio = document.createElement("div");
+        let abajo = document.createElement("div");
 
+
+
+        if (Arraysimbolos[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
+            arriba.style.color = 'red';
+            abajo.style.color = 'red';
+        }
+
+       
+        arriba.innerHTML = nuevosim[k];
+    medio.innerHTML = Arraynumeros[nuevonum[k]];
+        abajo.innerHTML = nuevosim[k];
+
+        
+        ///llama al style creado del css
+        padre.classList.add("creado");
+        arriba.classList.add("div1");
+        medio.classList.add("div2");
+        abajo.classList.add("div3");
+
+
+        padre.appendChild(arriba);
+        padre.appendChild(medio);
+        padre.appendChild(abajo);
+
+        cards.appendChild(padre);
+
+
+    
     }
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -112,6 +153,9 @@ mostrar.addEventListener("click", () => {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////selector sort
+
+    
+
     const selectSort = () => {
         let min = 0;
         while (min < nuevonum.length - 1) {
@@ -120,26 +164,60 @@ mostrar.addEventListener("click", () => {
                     let aux = nuevonum[min];
                     nuevonum[min] = nuevonum[m];
                     nuevonum[m] = aux;
-                }
-            }
-            min++;
+          }
+     
+                              min++;
         }
-        let lista2 = document.querySelector('#lista-numeros2');
-        for (let l = 0; l < nuevonum.length; l++) {
-            let item = document.createElement('li');
-            //item.innerHTML = nuevonum[l];
-            item.innerHTML = Arraynumeros[nuevonum[l]];
-            lista2.appendChild(item);
 
+       }  
+
+       for(h=0;h<nuevonum.length;h++){
+                 let padre = document.createElement("div");
+        let arriba = document.createElement("div");
+        let medio = document.createElement("div");
+        let abajo = document.createElement("div");
+
+        
+      
+        if (Arraysimbolos[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
+            arriba.style.color = 'red';
+            abajo.style.color = 'red';
         }
-    };
+        arriba.innerHTML = Arraysimbolos[simbolos];
+         medio.innerHTML = Arraynumeros[nuevonum[h]];
+        abajo.innerHTML = Arraysimbolos[simbolos];
+
+        
+        ///llama al style creado del css
+        padre.classList.add("creado");
+        arriba.classList.add("div1");
+        medio.classList.add("div2");
+        abajo.classList.add("div3");
+
+
+        padre.appendChild(arriba);
+        padre.appendChild(medio);
+        padre.appendChild(abajo);
+
+        cards.appendChild(padre);
+ 
+     }
+           
+      };
 
 
     let btn2 = document.querySelector('#btn2');
     btn2.addEventListener('click', selectSort);
 
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////
+      
+
+
     /////////////termina ordenar select
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 });
+
 
