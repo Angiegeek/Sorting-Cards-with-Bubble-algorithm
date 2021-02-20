11f0ea53c85c74d2d1389a587d41602268003570
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-20 23:28:25 +0000

    actuliazacion burbuja

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var Arraysimbolos = ["&#x2665", "\u2660", '&#x2663', '&#x2666'];
 nuevonum = [];
 nuevosim = [];
 
+
 var cantidadcartas = document.getElementById("texto");
 var mostrar = document.getElementById("btn3");
 var cards = document.getElementById("cards");
@@ -17,21 +18,26 @@ mostrar.addEventListener("click", () => {
     /////////////////cantidadcartas va ser la cantidad que escribe el usuario
     cantidadcartas = cantidadcartas.value;
     //alert(cantidadcartas);
-
+ArrayTotal=[];
+ArrayTotal2=[];
     for (let i = 0; i < cantidadcartas; i++) {
+         
 
         var numeros = Math.floor(Math.random() * (13));
+           
         nuevonum[i] = numeros;
-        //console.log(nuevonum[i]);
+        
+    
         var simbolos = Math.floor(Math.random() * (4));
-        console.log(simbolos);
-         //console.log(simbolos);
-        //nuevosim[i] = Arraysimbolos[simbolos];
-        //console.log(Arraysimbolos[simbolos]);
+        nuevosim[i] = simbolos;
 
-        //console.log(nuevosim);
-        //let sign = window.prompt("cuadro");
-        //falta poner colores 
+ 
+
+        //console.log("numero",nuevonum[i],"simbolo",nuevosim[i]);
+     
+
+
+
 
         let padre = document.createElement("div");
         let arriba = document.createElement("div");
@@ -49,7 +55,7 @@ mostrar.addEventListener("click", () => {
         medio.innerHTML = Arraynumeros[numeros];
         abajo.innerHTML = Arraysimbolos[simbolos];
 
-        
+
         ///llama al style creado del css
         padre.classList.add("creado");
         arriba.classList.add("div1");
@@ -63,9 +69,9 @@ mostrar.addEventListener("click", () => {
 
         cards.appendChild(padre);
 
-        //console.log(nuevonum[i]);
 
-}
+
+    }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /////////ordenamiento burbuja 
     let btn = document.querySelector('#btn');
@@ -75,89 +81,93 @@ mostrar.addEventListener("click", () => {
         var swapp;
         var n = nuevonum.length - 1;
         var x = nuevonum;
-       // console.log(nuevonum);
+        var y = nuevosim;
+
         do {
             swapp = false;
             for (var j = 0; j < n; j++) {
                 if (x[j] > x[j + 1]) {
+                    
+                    var temp1=y[j];
+                    y[j] = y[j + 1];
+                    y[j + 1] = temp1;
+                    
+                    
+                    
                     var temp = x[j];
                     x[j] = x[j + 1];
                     x[j + 1] = temp;
                     swapp = true;
+
+                   
+               
+
                 }
+           
             }
+
             n--;
+           // console.log(x);
         } while (swapp);
+         //console.log(x);
 
         let lista = document.querySelector('#lista-numeros');
         for (let k = 0; k < nuevonum.length; k++) {
             let item = document.createElement('li');
-            let item2=document.createElement('li');
-            item.innerHTML = nuevonum[k];
-      
-            item.innerHTML = Arraynumeros[nuevonum[k]];
-            
-          // console.log(nuevonum[k]);
-           // console.log(nuevosim[k]);
-            //nuevosim[k]=nuevosim[nuevonum[k]];
-            //console.log(nuevosim);
-            item2.innerHTML = nuevosim[nuevonum[k]];
-            
 
-   // arriba.innerHTML = Arraysimbolos[simbolos];
-     //    medio.innerHTML = Arraynumeros[nuevonum[h]];
-       // abajo.innerHTML = Arraysimbolos[simbolos];
-
-
-            //lista.appendChild(item);
-            //lista.appendChild(item2);
-      
-
-              //  console.log(nuevonum[k]);
-                
-       /////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
-/////////////////////pruebas
-    
-        let padre = document.createElement("div");
-        let arriba = document.createElement("div");
-        let medio = document.createElement("div");
-        let abajo = document.createElement("div");
-
-
-
-        if (Arraysimbolos[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
-            arriba.style.color = 'red';
-            abajo.style.color = 'red';
-        }
-
+            let item2 = document.createElement('li');
        
-     //   arriba.innerHTML = nuevosim[k];
-   // medio.innerHTML = Arraynumeros[nuevonum[k]];
-     //  abajo.innerHTML = nuevosim[k];
 
 
-   arriba.innerHTML = Arraysimbolos[simbolos];
-         medio.innerHTML = Arraynumeros[nuevonum[k]];
-        abajo.innerHTML = Arraysimbolos[simbolos];
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////pruebas
 
-        
-        ///llama al style creado del css
-        padre.classList.add("creado");
-        arriba.classList.add("div1");
-        medio.classList.add("div2");
-        abajo.classList.add("div3");
+            let padre = document.createElement("div");
+            let arriba = document.createElement("div");
+            let medio = document.createElement("div");
+            let abajo = document.createElement("div");
 
 
-        padre.appendChild(arriba);
-        padre.appendChild(medio);
-        padre.appendChild(abajo);
 
-        cards.appendChild(padre);
+            if (nuevosim[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
+                arriba.style.color = 'red';
+                abajo.style.color = 'red';
+            }
 
 
-    
-    }
+            
+           //console.log("nuevapos",k,"simbolob",nuevosim[[k]]);
+         
+
+           
+           //arriba.innerHTML = Arraysimbolos[simbolos];
+            //medio.innerHTML = Arraynumeros[nuevonum[k]];
+            //abajo.innerHTML = Arraysimbolos[simbolos];
+
+
+
+            arriba.innerHTML = Arraysimbolos[nuevosim[k]];
+            medio.innerHTML = Arraynumeros[nuevonum[k]];
+            abajo.innerHTML = Arraysimbolos[nuevosim[k]];
+
+
+            ///llama al style creado del css
+            padre.classList.add("creado");
+            arriba.classList.add("div1");
+            medio.classList.add("div2");
+            abajo.classList.add("div3");
+
+
+            padre.appendChild(arriba);
+            padre.appendChild(medio);
+            padre.appendChild(abajo);
+
+            cards.appendChild(padre);
+
+
+
+        }
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////termina burbuja
@@ -165,7 +175,7 @@ mostrar.addEventListener("click", () => {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////selector sort
 
-    
+
 
     const selectSort = () => {
         let min = 0;
@@ -173,48 +183,55 @@ mostrar.addEventListener("click", () => {
             for (let m = min + 1; m < nuevonum.length; m++) {
                 if (nuevonum[min] > nuevonum[m]) {
                     let aux = nuevonum[min];
+
+                    let aux2=nuevosim[min];
+
                     nuevonum[min] = nuevonum[m];
+
+                    nuevosim[min]=nuevosim[m];
+                   nuevosim[m]=aux2;
+
                     nuevonum[m] = aux;
-          }
-     
-                              min++;
+                }
+
+                min++;
+            }
+
         }
 
-       }  
+        for (h = 0; h < nuevonum.length; h++) {
+            let padre = document.createElement("div");
+            let arriba = document.createElement("div");
+            let medio = document.createElement("div");
+            let abajo = document.createElement("div");
 
-       for(h=0;h<nuevonum.length;h++){
-                 let padre = document.createElement("div");
-        let arriba = document.createElement("div");
-        let medio = document.createElement("div");
-        let abajo = document.createElement("div");
 
-        
-      
-        if (Arraysimbolos[simbolos] == Arraysimbolos[0] || Arraysimbolos[simbolos] == Arraysimbolos[3]) {
-            arriba.style.color = 'red';
-            abajo.style.color = 'red';
+
+            if (nuevosim[h] == Arraysimbolos[0] || nuevosim[h] == Arraysimbolos[3]) {
+                arriba.style.color = 'red';
+                abajo.style.color = 'red';
+            }
+            arriba.innerHTML = Arraysimbolos[nuevosim[h]];
+            medio.innerHTML = Arraynumeros[nuevonum[h]];
+            abajo.innerHTML = Arraysimbolos[nuevosim[h]];
+
+
+            ///llama al style creado del css
+            padre.classList.add("creado");
+            arriba.classList.add("div1");
+            medio.classList.add("div2");
+            abajo.classList.add("div3");
+
+
+            padre.appendChild(arriba);
+            padre.appendChild(medio);
+            padre.appendChild(abajo);
+
+            cards.appendChild(padre);
+
         }
-        arriba.innerHTML = Arraysimbolos[simbolos];
-         medio.innerHTML = Arraynumeros[nuevonum[h]];
-        abajo.innerHTML = Arraysimbolos[simbolos];
 
-        
-        ///llama al style creado del css
-        padre.classList.add("creado");
-        arriba.classList.add("div1");
-        medio.classList.add("div2");
-        abajo.classList.add("div3");
-
-
-        padre.appendChild(arriba);
-        padre.appendChild(medio);
-        padre.appendChild(abajo);
-
-        cards.appendChild(padre);
- 
-     }
-           
-      };
+    };
 
 
     let btn2 = document.querySelector('#btn2');
@@ -223,7 +240,7 @@ mostrar.addEventListener("click", () => {
 
     ////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////
-      
+
 
 
     /////////////termina ordenar select
